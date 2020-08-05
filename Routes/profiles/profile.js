37ab5299eba8553480663fb43dd8ad194124861c
chitty <geethavandadi123@gmail.com>
2020-08-05 21:34:53 +0530

    edit profile

--- Routes/profiles/profile.js
+++ Routes/profiles/profile.js
@@ -36,11 +36,44 @@ router.get("/user-details/:id", (req, res) => {
     })
     .catch((err) => console.log(err));
 });
+router.get("/edit-profile/:id", (req, res) => {
+  Profile.findOne({ _id: req.params.id })
+    .lean()
+    .then((edit_profile) => {
+      res.render("./profiles/edit-profile", { edit_profile });
+    })
+    .catch((err) => console.log(err));
+});
+
 
 
 // @http method POST
 // @description CREATE PROFILE DATA
 // @access PRIVATE
+router.put("/edit-profile/:id",upload.single("photo"),(req,res)=>{
+  Profile.findOne({_id:req.params.id})
+  .then(updateProfile=>{
+    //lefthand side old value=right handside its new value
+    updateProfile.photo=req.file;
+    updateProfile.firstname=req.body.firstname;
+    updateProfile.lastname=req.body.lastname;
+    updateProfile.phone=req.body.phone;
+    updateProfile.gender=req.body.gender;
+    updateProfile.designation=req.body.designation;
+    updateProfile.address=req.body.address;
+    updateProfile.alt_address=req.body.alt_address;
+    updateProfile.skills=req.body.skills;
+    updateProfile.country=req.body.country;
+    updateProfile.landmark=req.body.landmark;
+    updateProfile.pincode=req.body.pincode;
+    //update in database
+    updateProfile.save()
+    .then((update)=>{
+      res.redirect("/profile/user-details",201,{update});
+    })
+    .catch((err)=>console.log(err));
+  })
+})
 
 router.post("/create-profile", upload.single("photo"),(req, res) => {
   let {
